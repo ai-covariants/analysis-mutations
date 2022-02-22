2be073b47b014dcb4fa19d8510b4ba8418ea1ac6
clairekmy <62088478+clairekmy@users.noreply.github.com>
2022-02-22 22:33:28 +1100

    Add files via upload

---
+++ Thesis/Code/Ming Algorithm Template-2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from pysam import FastaFile
+import pandas as pd
+import random
+##key in the file name 
+
+
+# In[2]:
+
+
+##similarly for mers
+fasta_mers = "genomic.fna"
+mers = FastaFile(fasta_mers)
+##randomly choose one from the fai file created
+first_mers = mers['JX869059.2']
+first_mers
+
+
+# In[3]:
+
+
+##check the length of my sequence
+len(first_mers)
+
+
+# In[4]:
+
+
+mers_new = first_mers
+n = 2000
+while (n < 10000):
+    for sequence in mers_new[n]:
+        if sequence == 'A':
+            mers_new = mers_new[0:n]+'G'+mers_new[n+1:]
+        elif sequence =='G':
+            mers_new = mers_new[0:n]+'A'+mers_new[n+1:]
+        elif sequence =='C':
+            mers_new = mers_new[0:n]+'T'+mers_new[n+1:]
+        else:
+            mers_new = mers_new[0:n]+'C'+mers_new[n+1:]  
+    print(n, mers_new[n:n+900:100])
+    n += 1000
+        
+            
+print('Loop stop')
+
+
+# In[5]:
+
+
+##check with original sequence
+print(first_mers[2000:2900:100])
+print(first_mers[3000:3900:100])
+print(first_mers[4000:4900:100])
+print(first_mers[5000:5900:100])
+print(first_mers[6000:6900:100])
+print(first_mers[7000:7900:100])
+print(first_mers[8000:8900:100])
+print(first_mers[8000:8900:100])
+print(first_mers[9000:9900:100])
+
+
+# In[6]:
+
+
+def onechange(first_mers):
+    mers_new = first_mers
+    n = 2000
+    while (n < 10000):
+        for sequence in mers_new[n]:
+            if sequence == 'A':
+                mers_new = mers_new[0:n]+'G'+mers_new[n+1:]
+            elif sequence =='G':
+                mers_new = mers_new[0:n]+'A'+mers_new[n+1:]
+            elif sequence =='C':
+                mers_new = mers_new[0:n]+'T'+mers_new[n+1:]
+            else:
+                mers_new = mers_new[0:n]+'C'+mers_new[n+1:]  
+        print(n, mers_new[n:n+900:100])
+        n += 1000
+        
+            
+    print('Loop stop')
+    
+  
+
+
+# In[7]:
+
+
+ming = onechange(first_mers)
+ming
+
+
+# In[8]:
+
+
+def change(first_mers,x):
+    mers_new = first_mers
+    n = 2000
+    if x == 1:
+        while (n < 10000):
+            for sequence in mers_new[n]:
+                if sequence == 'A':
+                    mers_new = mers_new[0:n]+'G'+mers_new[n+1:]
+                elif sequence =='G':
+                    mers_new = mers_new[0:n]+'A'+mers_new[n+1:]
+                elif sequence =='C':
+                    mers_new = mers_new[0:n]+'T'+mers_new[n+1:]
+                else:
+                    mers_new = mers_new[0:n]+'C'+mers_new[n+1:]  
+            print(n, mers_new[n:n+900:100])
+            n += 1000
+    else:
+        while n < 10000:
+            for x in range(n,n+900,100):
+                for sequence in mers_new[x]:
+                    if sequence == 'A':
+                        mers_new = mers_new[0:x]+'G'+mers_new[x+1:]
+                    elif sequence =='G':
+                        mers_new = mers_new[0:x]+'A'+mers_new[x+1:]
+                    elif sequence =='C':
+                        mers_new = mers_new[0:x]+'T'+mers_new[x+1:]
+                    else:
+                        mers_new = mers_new[0:x]+'C'+mers_new[x+1:]  
+            print(n, mers_new[n:n+900:100])
+            n += 1000
+            
+    print('Loop stop')
+
+
+# In[9]:
+
+
+mming = change(first_mers,2)
+mming
+
