338b4dbfa5ab82864888a77ed9153a539a06f68d
clairekmy <62088478+clairekmy@users.noreply.github.com>
2022-02-22 19:36:45 +0800

    Update Ming Algorithm Template.py

--- Thesis/Code/Ming Algorithm Template.py
+++ Thesis/Code/Ming Algorithm Template.py
@@ -21,53 +21,7 @@ first_mers = mers['JX869059.2']
 first_mers
 
 
-# In[3]:
-
-
-##check the length of my sequence
-len(first_mers)
-
-
-# In[4]:
-
-
-mers_new = first_mers
-n = 2000
-while (n < 10000):
-    for sequence in mers_new[n]:
-        if sequence == 'A':
-            mers_new = mers_new[0:n]+'G'+mers_new[n+1:]
-        elif sequence =='G':
-            mers_new = mers_new[0:n]+'A'+mers_new[n+1:]
-        elif sequence =='C':
-            mers_new = mers_new[0:n]+'T'+mers_new[n+1:]
-        else:
-            mers_new = mers_new[0:n]+'C'+mers_new[n+1:]  
-    print(n, mers_new[n:n+900:100])
-    n += 1000
-        
-            
-print('Loop stop')
-
-
-# In[5]:
-
-
-##check with original sequence
-print(first_mers[2000:2900:100])
-print(first_mers[3000:3900:100])
-print(first_mers[4000:4900:100])
-print(first_mers[5000:5900:100])
-print(first_mers[6000:6900:100])
-print(first_mers[7000:7900:100])
-print(first_mers[8000:8900:100])
-print(first_mers[8000:8900:100])
-print(first_mers[9000:9900:100])
-
-
-# In[6]:
-
-
+##one mutation
 def onechange(first_mers):
     mers_new = first_mers
     n = 2000
@@ -87,10 +41,6 @@ def onechange(first_mers):
             
     print('Loop stop')
     
-  
-
-
-# In[7]:
 
 
 ming = onechange(first_mers)
@@ -99,7 +49,7 @@ ming
 
 # In[8]:
 
-
+##option for one mutation or ten mutations
 def change(first_mers,x):
     mers_new = first_mers
     n = 2000
